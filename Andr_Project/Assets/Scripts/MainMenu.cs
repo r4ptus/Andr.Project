4c277ea3cd10cd14b5bf97540a56178831634257
Raptus <lee@familiehartwich.de>
2018-12-03 16:05:42 +0100

    Initial Commit
    first iteration of the main menu

---
+++ Andr_Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    /// <summary>
+    /// Function to Start the game
+    /// loads the next scene in the queue
+    /// </summary>
+	public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Function to quit the app
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Function for setting the volume of the game
+    /// </summary>
+    /// <param name="volume"></param>
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
